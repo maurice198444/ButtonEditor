60924bda9d435dd0da3f3cc79954d4f0e5d5239c
Maurice <maurice.st@icloud.com>
2025-11-29 06:33:33 +0100

    Routes erstellt und Datenbankeinträge erstellt

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CardController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -7,3 +8,7 @@ use Inertia\Inertia;
 Route::inertia('/', 'Home', ['user' => 'Maurice']);
 
 Route::inertia('/about', 'About');
+
+
+Route::get('/cards/{card}', [CardController::class, 'show']);
+Route::post('/cards/{card}/versions', [CardController::class, 'storeVersion']);
